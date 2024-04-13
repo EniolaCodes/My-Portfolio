a40025e9db70b1dc0b7b778238e1a2504fd49e47
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-04-13 19:22:21 +0100

    fix the navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useState} from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from "react-icons/ai";
@@ -6,6 +6,11 @@ import {FaGithub, FaLinkedinIn} from "react-icons/fa";
 import {BsFillPersonLinesFill} from "react-icons/bs";
 
 const Navbar = () => {
+  const [nav , setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
@@ -33,13 +38,17 @@ const Navbar = () => {
             <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
           </Link>
         </ul>
-        <div className='md:hidden'>
+        <div onClick={handleNav} className='md:hidden'>
           <AiOutlineMenu size={25}/>
         </div>
       </div>
     </div>
-    <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
-      <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+    <div className={nav ? `md:hidden fixed left-0 top-0 w-full h-screen bg-black/70` : ""}>
+      <div className={
+        nav
+        ? `fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500`
+        : `fixed left-[-100%] top-0 p-10 ease-in duration-500`
+      }>
         <div>
           <div className='flex w-full items-center justify-between'>
             <Image 
@@ -48,7 +57,7 @@ const Navbar = () => {
               width= "90" 
               height="35"
           />
-          <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+          <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
             <AiOutlineClose/>
           </div>
         </div>
@@ -75,7 +84,7 @@ const Navbar = () => {
             </Link>
           </ul>
           <div className='pt-40'>
-            <p className='uppercase tracking-widest text-[#5651e5]'>Let's Connect</p>
+            <p className='uppercase tracking-widest text-[#D3A43E]'>Let&#39;s Connect</p>
             <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
               <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
                 <FaLinkedinIn/>
