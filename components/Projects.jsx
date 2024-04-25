8799b81108d2cbc10861b9a6208e206e4b489f25
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-04-25 12:23:55 +0100

    add smooth scrolling

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,7 +4,7 @@ import LogisticsImg from '../public/assets/projects/logistics.PNG';
 
 const Projects = () => {
   return (
-    <div className="w-full">
+    <div id='projects' className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="uppercase text-xl tracking-widest text-[#D3A43E]">Projects</p>
         <h2 className="py-4">What I Have Built</h2>
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -13,7 +13,7 @@ import Firebase from '../public/assets/skills/firebase.png';
 
 const Skills = () => {
   return (
-    <div className='w-full lg:h-screen p-2'>
+    <div id='skills' className='w-full lg:h-screen p-2'>
       <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
       <p className='uppercase text-xl tracking-widest text-[#D3A43E]'>Skills</p>
       <h2 className='py-4'>What I Can Do</h2>
