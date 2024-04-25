1450c17f48ca2ba8c05678ef267ffb37f14e5e07
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-04-25 11:55:32 +0100

    add the contact component

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,9 @@ import Main from "../components/Main";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
+import Contact from "@/components/Contact";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -22,6 +23,7 @@ export default function Home() {
       <About/>
       <Skills/>
       <Projects/>
+      <Contact/>
    </div>
   );
 }
