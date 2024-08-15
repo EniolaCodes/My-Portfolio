1aa1ac66d69ade7e638d97ccac24d128ede116ba
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2024-08-15 13:05:00 +0100

    add the environment variable in the vercel project

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 	if (req.method === "POST") {
 		const { name, email, subject, message } = req.body;
 
-		// Create a transporter object
+		
 		const transporter = nodemailer.createTransport({
 			service: "gmail",
 			auth: {
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 			},
 		});
 
-		// Define email options
+		
 		const mailOptions = {
 			from: email,
 			to: process.env.EMAIL_USER, 
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
 		};
 
 		try {
-			// Send email
+			
 			await transporter.sendMail(mailOptions);
 			res.status(200).json({ success: true });
 		} catch (error) {
