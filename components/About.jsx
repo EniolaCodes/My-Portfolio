fe45e0d6ee106d81cffcd876038d0b16dee566aa
EniolaCodes <adebimpeniola@gmail.com>
2026-03-13 20:39:44 +0100

    change favicon.ico

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import ProfilePic from "../public/assets/profile_pic.png";
+import ProfilePic from "../public/assets/eniola-red.jpeg";
 import Link from "next/link";
 
 const About = () => {
